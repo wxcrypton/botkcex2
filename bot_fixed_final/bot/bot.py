2eeffcc41aaee566727ba1e4be6f59aa930af25c
wxcrypton <hajamiir85@gmail.com>
2025-02-22 16:14:18 +0330

    Update bot.py

--- bot_fixed_final/bot/bot.py
+++ bot_fixed_final/bot/bot.py
@@ -193,5 +193,12 @@ async def main():
     # اجرای ربات
     await application.run_polling()
 
-if __name__ == '__main__':
-    asyncio.run(main())
+if __name__ == "__main__":
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(main())
+
